715c7759aca3d0099f209756d4932b20e0d7df1b
X <androysol@gmail.com>
2023-03-08 21:37:56 +0200

    feat: registration, authorisation
    Possibility of user registration and authorisation, created strategy,
    configurators, decorators

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule, ConfigModule.forRoot()],
+	controllers: [AppController],
+	providers: [AppService, PrismaService]
 })
 export class AppModule {}
---
+++ server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator'
+
+export class AuthDto {
+	@IsEmail()
+	email: string
+
+	@IsString()
+	@MinLength(6, {
+		message: 'Password must be at least 6 characters long'
+	})
+	@MaxLength(20)
+	password: string
+}
